54aa73c1f8bf98b366b1a59307f96e997193c24f
Serhii Pylypenko <jpee.dev@gmail.com>
2021-11-23 12:57:47 +0200

    add 'Escape' listener for menu

--- src/menu/script.js
+++ src/menu/script.js
@@ -1,32 +1,42 @@
-const $nav = document.querySelector('.navigation');
-const $button = document.querySelector('.navigation__toggle');
+const menuApp = () => {
+  const $nav = document.querySelector('.navigation');
+  const $button = document.querySelector('.navigation__toggle');
 
-const switchAttributes = () => {
-  if ($nav.classList.contains('navigation--shown')) {
-    // open menu
-    $button.setAttribute('aria-expanded', 'true');
-    $button.setAttribute('aria-label', 'close menu');
-  } else {
-    // close menu
-    $button.setAttribute('aria-expanded', 'false');
-    $button.setAttribute('aria-label', 'open menu');
-  }
-};
+  const switchAttributes = () => {
+    if ($nav.classList.contains('navigation--shown')) {
+      // open menu
+      $button.setAttribute('aria-expanded', 'true');
+      $button.setAttribute('aria-label', 'close menu');
+    } else {
+      // close menu
+      $button.setAttribute('aria-expanded', 'false');
+      $button.setAttribute('aria-label', 'open menu');
+    }
+  };
 
-const closeMenu = () => {
-  $nav.classList.remove('navigation--shown');
-  switchAttributes();
-};
-
-document.addEventListener('click', (evt) => {
-  if (evt.target.closest('.navigation__toggle')) {
-    $nav.classList.toggle('navigation--shown');
+  const closeMenu = () => {
+    $nav.classList.remove('navigation--shown');
     switchAttributes();
-  }
+  };
 
-  if (!evt.target.closest('.navigation')) {
-    closeMenu();
-  }
-});
+  document.addEventListener('click', (evt) => {
+    if (evt.target.closest('.navigation__toggle')) {
+      $nav.classList.toggle('navigation--shown');
+      switchAttributes();
+    }
 
-closeMenu();
+    if (!evt.target.closest('.navigation')) {
+      closeMenu();
+    }
+  });
+
+  document.addEventListener('keyup', (evt) => {
+    if (evt.code === 'Escape') {
+      closeMenu();
+    }
+  });
+
+  closeMenu();
+};
+
+menuApp();
